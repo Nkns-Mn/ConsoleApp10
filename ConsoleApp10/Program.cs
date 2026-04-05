dfbdfcc94d4d5d501ee9dc2af65d734d7ce31938
mana.tokorocchi8@gmail.com <mana.tokorocchi8@gmail.com>
2026-04-06 00:49:26 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestInfo info = new TestInfo("山田",20,170,60);
+            info.ShowPerson();
+            info.ShowInfo();
+        }
+    }
+    class TestPerson
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public TestPerson (string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void ShowPerson()
+        {
+            Console.WriteLine("基底クラス");
+            Console.WriteLine($"名前：{name},年齢：{age}");
+        }
+        }
+
+    class TestInfo : TestPerson
+    {
+        public int height { get; set; }
+        public int weight { get; set; }
+        public TestInfo(string name, int age, int height, int weight)
+           : base(name, age)
+        {
+            this.height = height;
+            this.weight = weight;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("派生クラス");
+            Console.WriteLine($"身長：{height},体重：{weight}");
+        }
+    }
+}
